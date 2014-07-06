6555e89dac9a3c713b0e6de5fe4bc2b577932fd4
Jakob Schumann <schumann@vrok.de>
2014-07-06 18:02:53 +0200

    upd: cleanup

--- src/Vrok/SlmQueue/Job/ExitWorker.php
+++ src/Vrok/SlmQueue/Job/ExitWorker.php
@@ -1,21 +1,29 @@
 <?php
+/**
+ * @copyright   (c) 2014, Vrok
+ * @license     http://customlicense CustomLicense
+ * @author      Jakob Schumann <schumann@vrok.de>
+ */
 
 namespace Vrok\SlmQueue\Job;
 
 use SlmQueue\Job\AbstractJob;
 
 /**
- * We have no access to the supervisorctl from the web interface or the console as we
- * are in a chrooted environment. But we still need a method to restart the application
- * to reload changed configuration / changed code.
- * This job simply executes an exit which stops the executing worker so it is restarted
- * by supervisord.
- * We need to send it to the queue multiple times so it is processed by hopefully all
- * available workers before the restarted ones take over again. So there is no guarantee
- * all workers are really reloaded.
+ * This job simply executes an exit which stops the executing worker.
+ *
+ * Can be used if workers are handled & restarted by supervisord to reload changed
+ * configuration / changed code.
+ * Only useful when there is only one worker assigned for the queue as we can not
+ * guarantee that each worker processes one ExitWorker job even if we send multiple jobs
+ * to the queue because there may be other long running processes so a restarted worker
+ * catches another ExitWorker job again.
  */
 class ExitWorker extends AbstractJob
 {
+    /**
+     * {@inheritdoc}
+     */
     public function execute()
     {
         exit;
