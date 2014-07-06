42d6c5c0d7689487f066e185a140f24cbb3c704e
Jakob Schumann <schumann@vrok.de>
2014-07-06 18:09:48 +0200

    fix: config path

--- src/Vrok/Module.php
+++ src/Vrok/Module.php
@@ -49,7 +49,7 @@ class Module implements
      */
     public function getConfig()
     {
-        return include __DIR__ . '/config/module.config.php';
+        return include __DIR__ . '/../../config/module.config.php';
     }
 
     /**
