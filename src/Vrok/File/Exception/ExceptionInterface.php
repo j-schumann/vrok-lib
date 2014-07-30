23fb7e79b39f7ab19786e71033edc2767621ade9
www-data <www-data@vrok.de>
2014-07-30 16:04:08 +0200

    fix: lazy listeners add: File

---
+++ src/Vrok/File/Exception/ExceptionInterface.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @copyright   (c) 2014, Vrok
+ * @license     http://customlicense CustomLicense
+ * @author      Jakob Schumann <schumann@vrok.de>
+ */
+
+namespace Vrok\File\Exception;
+
+/**
+ * Marker interface to allow catching of exceptions from the file component.
+ */
+interface ExceptionInterface
+{}
