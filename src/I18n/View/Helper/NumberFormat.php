055f87e7bc47b89c3cbb07abb0103973fc3e9bcf
Jakob Schumann <schumann@vrok.de>
2016-03-29 09:17:34 +0200

    fix: numberFormat helper to jsonencode  in case its an array

--- src/I18n/View/Helper/NumberFormat.php
+++ src/I18n/View/Helper/NumberFormat.php
@@ -54,7 +54,7 @@ class NumberFormat extends ZendFormat
         }
 
         $formatterId = md5(
-            $formatStyle . "\0" . $locale . "\0" . $decimals . "\0"
+            $formatStyle . "\0" . $locale . "\0" . json_encode($decimals) . "\0"
             . md5(serialize($textAttributes))
         );
 
