afaa5128457d44936e209580dbaacf89b4b67946
Jakob Schumann <schumann@vrok.de>
2016-03-30 10:27:51 +0200

    upd: copyright year

--- src/Form/Element/ObjectHidden.php
+++ src/Form/Element/ObjectHidden.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @copyright   (c) 2014, Vrok
+ * @copyright   (c) 2014-16, Vrok
  * @license     http://customlicense CustomLicense
  * @author      Jakob Schumann <schumann@vrok.de>
  */
