a32352e0d450fe04378f2b204b2087d71ffb753c
Jakob Schumann <schumann@vrok.de>
2016-03-30 11:10:41 +0200

    upd: license

--- src/Form/Element/ObjectHidden.php
+++ src/Form/Element/ObjectHidden.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @copyright   (c) 2014-16, Vrok
- * @license     http://customlicense CustomLicense
+ * @license     MIT License (http://www.opensource.org/licenses/mit-license.php)
  * @author      Jakob Schumann <schumann@vrok.de>
  */
 
