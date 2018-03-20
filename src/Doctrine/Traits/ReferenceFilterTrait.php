ea77f9b844e9c2a4c0e4237da6255d98bc7cc873
Jakob Schumann <schumann@vrok.de>
2018-03-20 15:50:52 +0100

    add: ReferenceFilterTrait

---
+++ src/Doctrine/Traits/ReferenceFilterTrait.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * @copyright   (c) 2018, Vrok
+ * @license     MIT License (http://www.opensource.org/licenses/mit-license.php)
+ * @author      Jakob Schumann <schumann@vrok.de>
+ */
+
+namespace Vrok\Doctrine\Traits;
+
+/**
+ * Used to extend Vrok\Doctrine\AbstractFilter to allow simple filtering by
+ * reference.
+ */
+trait ReferenceFilterTrait
+{
+    /**
+     * Uses the column:value pairs returned by the Vrok\References\ReferenceHelper
+     * to filter for matching entities.
+     *
+     * @param array $filterValues
+     *
+     * @return self
+     */
+    public function byReference(array $filterValues)
+    {
+        $conditions = [];
+        foreach($filterValues as $column => $value) {
+            if ($value) {
+                $conditions[] = "$this->alias.$column = :$column";
+                $this->qb->setParameter($column, $value);
+            } else {
+                $conditions[] = $this->qb->expr()->isNull("$this->alias.$column");
+            }
+        }
+
+        $this->qb->andWhere('('.implode(' AND ', $conditions).')');
+        return $this;
+    }
+}
